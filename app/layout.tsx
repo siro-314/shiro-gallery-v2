744e2366a40b2408c3c26ac5975604db7376b2c0
siro-314 <1126ailove@gmail.com>
2025-09-03 12:34:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,6 @@ export default function RootLayout({
                                                                 </main>main>
                                                     </div>div>
                                           </body>body>
-                                  </html>html>
+                                  </html>
                             )
 }</html>
